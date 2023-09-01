3d6c42d9dcd10b2d4628d5596fdc518881572469
Park-ChanHwi <x4cf7c3an2t@naver.com>
2023-09-01 12:22:46 +0900

    9-1 love

--- src/main/java/bankers/fisa/controller/MainController.java
+++ src/main/java/bankers/fisa/controller/MainController.java
@@ -73,6 +73,209 @@ public class MainController {
 		}
 	}
 
+@GetMapping("/bankersemp")
+	public ModelAndView bankersempPage() {
+		URI uri = UriComponentsBuilder.fromUriString("http://localhost:7070").path("/center/bankersemp").encode()
+				.build().toUri();
+		System.out.println("실행됨");
+		MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(uri, parameters, String.class);
+
+		String str = responseEntity.getBody().toString();
+		ModelAndView mv = new ModelAndView();
+		String[] strArr = str.split("/");
+		
+//------------------------------------------------------------------
+		//반복문 사용
+		for (int i = 0; i < strArr.length; i++) {
+		    String employeeSet = strArr[i];
+		    String[] sArr = employeeSet.split("_");
+		    
+		    if (sArr.length >= 0) {
+		        String setNum = sArr[0];
+		        String setName = sArr[2];
+		        String setPhoneNumber = sArr[1];
+		        String setID = sArr[3];
+		        String setPW = sArr[4];
+		        String setStatus = sArr[5];
+		        
+		        String setNumKey = "Set" + (i +1) + "Num";
+		        String setNameKey = "Set" + (i +1) + "Name";
+		        String setPhoneNumberKey = "Set" + (i+1) + "PhoneNumber";
+		        String setIDKey = "Set" + (i+1) + "ID";
+		        String setPWKey = "Set" + (i+1) + "PW";
+		        String setStatusKey = "Set" + (i+1) + "Status";
+		        
+		        mv.addObject(setNumKey, setNum);
+		        mv.addObject(setNameKey, setName);
+		        mv.addObject(setPhoneNumberKey, setPhoneNumber);
+		        mv.addObject(setIDKey, setID);
+		        mv.addObject(setPWKey, setPW);
+		        mv.addObject(setStatusKey, setStatus);
+		    }
+		}
+		//해결해야 할 사항 : 4번 영업사원의 이름과 전화번호가 서로 바뀌어서 출력이 됨!!
+		
+
+
+//------------------------------------------------------------------
+		// 첫번째 세트(사원1)
+		if (strArr.length >= 0) {
+			String firstSet = strArr[0];
+			String[] sArr1 = firstSet.split("_");
+
+			if (sArr1.length >= 2) {
+				
+				// 여기부터 첫번째사원 데이터
+				// 번호
+				String Set1Num = sArr1[0];
+				System.out.println(Set1Num);
+				mv.addObject("Set1Num", Set1Num);
+
+				// 이름
+				String Set1Name = sArr1[2];
+				System.out.println(Set1Name);
+				mv.addObject("Set1Name", Set1Name);
+				// 전화번호
+				String Set1PhoneNumber = sArr1[1];
+				System.out.println(Set1PhoneNumber);
+				mv.addObject("Set1PhoneNumber", Set1PhoneNumber);
+				// ID
+				String Set1ID = sArr1[3];
+				System.out.println(Set1ID);
+				mv.addObject("Set1ID", Set1ID);
+				// PW
+				String Set1PW = sArr1[4];
+				System.out.println(Set1PW);
+				mv.addObject("Set1PW", Set1PW);
+				// 상태(admin or emp인 경우 ->재직, / 그 외는 휴직)
+				String Set1Status = sArr1[5];
+				System.out.println(Set1Status);
+				mv.addObject("Set1Status", Set1Status);
+				// 버튼1
+
+				// 버튼2
+
+				// 버튼3
+
+			}
+		}
+
+//-------------------------------------------------------------------------	    
+		// 두번째 세트(사원2)
+	    if (strArr.length >= 2) {
+	        String secondSet = strArr[1];
+	        String[] sArr2 = secondSet.split("_");
+
+	        if (sArr2.length >= 4) {
+				// 여기부터 두번째사원 데이터
+				// 번호
+				if (sArr2.length >= 0) {
+					String Set2Num = sArr2[0];
+					System.out.println(Set2Num);
+					mv.addObject("Set2Num", Set2Num);
+				}
+				// 이름
+				if (sArr2.length >= 0) {
+					String Set2Name = sArr2[2];
+					System.out.println(Set2Name);
+					mv.addObject("Set2Name", Set2Name);
+				}
+
+				// 전화번호
+				if (sArr2.length >= 0) {
+					String Set2PhoneNumber = sArr2[1];
+					System.out.println(Set2PhoneNumber);
+					mv.addObject("Set2PhoneNumber", Set2PhoneNumber);
+				}
+				// ID
+				if (sArr2.length >= 0) {
+					String Set2ID = sArr2[3];
+					System.out.println(Set2ID);
+					mv.addObject("Set2ID", Set2ID);
+				}
+				// PW
+				if (sArr2.length >= 0) {
+					String Set2PW = sArr2[4];
+					System.out.println(Set2PW);
+					mv.addObject("Set2PW", Set2PW);
+				}
+				// 상태(admin or emp인 경우 ->재직, / 그 외는 휴직)
+				if (sArr2.length >= 0) {
+					String Set2Status = sArr2[5];
+					System.out.println(Set2Status);
+					mv.addObject("Set2Status", Set2Status);
+				}
+				// 버튼1
+
+				// 버튼2
+
+				// 버튼3
+
+			}
+		}
+
+//-------------------------------------------------------------------------	    
+		// 두번째 세트(사원3)
+	    if (strArr.length >= 3) {
+	        String thirdSet = strArr[2];
+	        String[] sArr3 = thirdSet.split("_");
+
+	        if (sArr3.length >= 6) {
+				// 여기부터 두번째사원 데이터
+				// 번호
+				if (sArr3.length >= 0) {
+					String Set3Num = sArr3[0];
+					System.out.println(Set3Num);
+					mv.addObject("Set3Num", Set3Num);
+				}
+				// 이름
+				if (sArr3.length >= 0) {
+					String Set3Name = sArr3[2];
+					System.out.println(Set3Name);
+					mv.addObject("Set3Name", Set3Name);
+				}
+
+				// 전화번호
+				if (sArr3.length >= 0) {
+					String Set3PhoneNumber = sArr3[1];
+					System.out.println(Set3PhoneNumber);
+					mv.addObject("Set3PhoneNumber", Set3PhoneNumber);
+				}
+				// ID
+				if (sArr3.length >= 0) {
+					String Set3ID = sArr3[3];
+					System.out.println(Set3ID);
+					mv.addObject("Set3ID", Set3ID);
+				}
+				// PW
+				if (sArr3.length >= 0) {
+					String Set3PW = sArr3[4];
+					System.out.println(Set3PW);
+					mv.addObject("Set3PW", Set3PW);
+				}
+				// 상태(admin or emp인 경우 ->재직, / 그 외는 휴직)
+				if (sArr3.length >= 0) {
+					String Set3Status = sArr3[5];
+					System.out.println(Set3Status);
+					mv.addObject("Set3Status", Set3Status);
+				}
+				// 버튼1
+
+				// 버튼2
+
+				// 버튼3
+
+			}
+		}
+//-------------------------------------------------------------------
+		mv.setViewName("employmanagement");
+
+		return mv;
+
+	}
+
 	@GetMapping("/newcustomer")
 	public ModelAndView newCustomerPage() {
 		ModelAndView mv = new ModelAndView();
